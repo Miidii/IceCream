d97bb7c9b71d0ae93f44058c05e3d4c6b5af901a
Alen Liang <simplyaalen@gmail.com>
2019-11-19 11:33:30 +0800

    修复 Warnings

--- IceCream/Classes/Manifest.swift
+++ IceCream/Classes/Manifest.swift
@@ -10,23 +10,29 @@ import Foundation
 /// This file is for setting some develop configs for IceCream framework.
 
 public class IceCream {
-    
+
     public static let shared = IceCream()
 
-    public var print: ((_ items: Any..., String, String) -> Void) = { items, separator, terminator in
-        #if DEBUG
-        var i = items.startIndex
-        repeat {
-            Swift.print(items[i], separator: separator, terminator: i == (items.endIndex - 1) ? terminator : separator)
-            i += 1
-        } while i < items.endIndex
-        #endif
-    }
+    /// There are quite a lot `print`s in the IceCream source files.
+    /// If you don't want to see them in your console, just set `enableLogging` property to false.
+    /// The default value is true.
+    public var enableLogging: Bool = true
 
+    public var print = { (items: Any, separator: String, terminator: String) -> Void in
+        Swift.print(items, separator: separator, terminator: terminator)
+    }
 }
 
 /// If you want to know more,
 /// this post would help: https://medium.com/@maxcampolo/swift-conditional-logging-compiler-flags-54692dc86c5f
 internal func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    IceCream.shared.print(items, separator: separator, terminator: terminator)
+    if (IceCream.shared.enableLogging) {
+        #if DEBUG
+        var i = items.startIndex
+        repeat {
+            IceCream.shared.print(items[i], separator, i == (items.endIndex - 1) ? terminator : separator)
+            i += 1
+        } while i < items.endIndex
+        #endif
+    }
 }
