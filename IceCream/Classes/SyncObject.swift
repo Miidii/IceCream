395386ad2cb6a59f4aa9f788d8e4f0930da7ac40
Alen Liang <simplyaalen@gmail.com>
2020-05-14 17:27:20 +0800

    初始化的时候同时同步删除修改

--- IceCream/Classes/SyncObject.swift
+++ IceCream/Classes/SyncObject.swift
@@ -150,8 +150,9 @@ extension SyncObject: Syncable {
     
     public func pushLocalObjectsToCloudKit() {
         let realm = try! Realm(configuration: self.realmConfiguration)
-        let recordsToStore: [CKRecord] = realm.objects(T.self).filter { !$0.isDeleted }.map { $0.record }
-        pipeToEngine?(recordsToStore, [])
+        let recordsToStore = realm.objects(T.self).filter { !$0.isDeleted }.map { $0.record }
+        let recordsIDsToDelete: [CKRecord] = realm.objects(T.self).filter { $0.isDeleted }.map { $0.recordID }
+        pipeToEngine?(recordsToStore, recordsIDsToDelete)
     }
     
 }
